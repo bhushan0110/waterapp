d24f1209000dd4956df7a15075f182768d4f8e53
bhushan0110 <baribhushan9120@gmail.com>
2023-05-05 22:40:10 +0530

    Commit

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const { BigQuery } = require('@google-cloud/bigquery');
 const Reading = require('./modals/readings');
 
 
+
 connectToDB();
+
 
 const app = express();
 const bigquery = new BigQuery({
@@ -13,6 +15,40 @@ const bigquery = new BigQuery({
 });
 
 app.use(express.json());
+
+//save to database
+app.post('/saveData', async (req, res) => {
+  try {
+    const min = 10
+    const max = 16
+    const fnum = 10
+    let i = 0
+    while (i < fnum) {
+      randomNum = Math.floor(Math.random() * ((max - min + 1)) * 1000) + min * 100;
+      const volume = randomNum //taking in litres 
+      const flatNo = (i + 1)
+      console.log(volume, "---", flatNo)
+      const myData = new Reading({
+        flatNo: flatNo,
+        volume: volume,
+        
+      });
+      myData.save()
+        .then(() => {
+          console.log('Document saved');
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+
+      i++
+    }
+    res.send('Done');
+  }
+  catch (err) {
+    console.log(err);
+  }
+})
 
 app.post('/stream-to-bigquery', (req, res) => {
   const { rows, datasetId, tableId } = req.body;
